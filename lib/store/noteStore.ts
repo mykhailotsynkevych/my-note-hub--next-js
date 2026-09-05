073674683f1df34e35b732a35d2c2c6ed82ea9e7
Mykhailo Tsynkevych <tsynkevych@gmx.de>
2026-09-05 13:05:08 +0200

    build fix

--- lib/store/noteStore.ts
+++ lib/store/noteStore.ts
@@ -1,6 +1,6 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
-import { NewNoteData } from '../api/api';
+import { NewNoteData } from '../api/clientApi';
 
 type NoteDraftStore = {
   draft: NewNoteData;
